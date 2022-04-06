28695a79e55f430b858d1d792bfcc46c3f358f53
dilaranuraltiparmak <78371701+dilaranuraltiparmak@users.noreply.github.com>
2022-04-06 14:07:18 +0300

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,11 @@
+﻿int time = DateTime.Now.Hour;
+if(time>6 && time<11)
+    Console.WriteLine("Günaydın");
+if (time <= 18)
+    Console.WriteLine("İyi Günler");
+else
+    Console.WriteLine("İyi Geceler");
+string sonuc = time <= 18 ? "İyi Günler " : "İyi Geceler";
+Console.WriteLine(sonuc);
+sonuc = time <= 11 ? "Günaydın": time<=18 ?"İyi Günler " : "İyi Geceler";
+Console.WriteLine(sonuc);
